ec2b1c9dc20a5b9f9b12d84c1baeee58accb0add
Rajashekar D <ras.d0019190@gmail.com>
2023-11-20 20:31:02 +0530

    running login in different file requests.js

--- first.js
+++ first.js
@@ -1,36 +1,7 @@
-const fs = require('fs')
 const http = require('http');
 
-const server = http.createServer ((req, res) =>{
+const route = require('./requests');
 
-    const url= req.url;
-    const method = req.method;
-
-    if (url == '/'){
-      res.write('<html>');
-      res.write('<head><title>my_first_page</title></head>'); 
-      res.write('<body><form action="/message" method="POST"><input type="text" name="value"><button>submit</button></form></body>');
-      res.write('</html>');
-      return res.end();
-    }
-
-     res.setHeader('Content-Type','text/html'); 
-
-  
-    if (url =='/message' && method =='POST' ){
-        fs.writeFileSync('message.txt','dummy');
-        res.statusCode = 302;
-        res.setHeader('Location','/');
-        return res.end();
-    }
-
-     res.write('<html>');
-     res.write('<head><title>my_first_page</title></head>');
-     res.write('<body><h1>hello world</h1></body>');
-     res.write('</html>');
-     res.end(); 
-    
-
-});
+const server = http.createServer (route.handler);
  
 server.listen(3000);

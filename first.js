8a24f1fde8492e385efb76239752371ddb5cb90f
Rajashekar D <ras.d0019190@gmail.com>
2023-11-21 20:40:32 +0530

    started using express

--- first.js
+++ first.js
@@ -1,7 +1,26 @@
 const http = require('http');
+// we use express js from now
 
-const route = require('./requests');
+const express = require('express');
+// this exports a function , so we import it
 
-const server = http.createServer (route.handler);
+const app = express();
+
+app.use((req, res, next )=>{
+    console.log('in middle ware');
+    next();//same as parameter // this allows to exit from one middle ware to another middelware
+    // without giving response to website
+});
+
+app.use((req, res, next )=>{
+    console.log('in another middle ware');
+    // we don,t need to set header as node because express does it for us
+    res.send('./hacka/index.html');
+});
+
+const server = http.createServer (app);
  
 server.listen(3000);
+
+
+// npm install --save express to get express for our project it will be added as dependencies tour package.json file

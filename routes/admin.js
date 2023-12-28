fd666559262f826efcef55a3e61d12f107035350
Rajashekar D <ras.d0019190@gmail.com>
2023-12-29 00:58:21 +0530

    now renderig pug files insted of static html files which give dynamic content to yhe page

--- routes/admin.js
+++ routes/admin.js
@@ -7,16 +7,15 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product',(req, res, next )=>{ 
-    res.sendFile(path.join(__dirname,'..','views','add_product.html'));
+   // res.sendFile(path.join(__dirname,'..','views','add_product.html'));
+   res.render('add_product',{pagetitle:'Add Product'});
 });
 // '/admin/add-product' since we adde filter to seperate routes more specifically so action in form changened
 
 // /admin/add-product => POST
 router.post('/add-product',(req, res, next )=>{ 
-   // console.log(req.body);
-   //products.push({title:req.body}); this will give all data in form of a directory
-   products.push({title:req.body.title});// this gets specific data from req
-   /// imp /// since this products variable is local to server, even if other user type url in other machine this data remain same for all since server is still running continuosly tis is not good this may alter their page due to this data maybe an email entrered in textbox filled to other user also so we must use other method
+   // console.log(req.body); 
+   products.push({title:req.body.title});
    //console.log(products);
     res.redirect('/');
 });
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,13 @@ const path = require('path');
 const express = require('express');
 const admindata = require('./admin')
 
-const routes = express();
+const routes = express.Router();
 
 routes.get('/',(req, res, next )=>{
-    console.log(admindata.products);
-    res.sendFile(path.join(__dirname,'..','views','shop.html'));
+    // console.log(admindata.products);
+    // res.sendFile(path.join(__dirname,'..','views','shop.html'));
+    const products = admindata.products;
+    res.render('shop',{prods: products, doctitle:'shop'});
 });
 
 module.exports = routes;
